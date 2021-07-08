22cfcd2915eeb6ad302bcdfa2bf7a895ee410cca
mahesh0605 <72660810+mahesh0605@users.noreply.github.com>
2021-07-08 18:30:53 +0530

    Add files via upload

---
+++ area of circle (1).py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# task 1 - Write a python program which accepts the radius of a circle from the user and computes area
+
+from math import pi
+
+r = float(input ("Enter radius of circle : "))
+
+print ("Area of the circle is: " + str(pi * r**2))
+
+
+# In[ ]:
+
+
+
+
---
+++ extension of file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# task 2 - Write a python program which accept a filename from the user and print the extension of that
+
+fn= input("Enter Filename: ")
+
+f = fn.split(".")
+
+print ("Extension of the file is : " + f[-1])
+
+
+# In[ ]:
+
+
+
+
